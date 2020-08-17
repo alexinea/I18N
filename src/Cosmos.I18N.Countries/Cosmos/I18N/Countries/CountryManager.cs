b4e8f7882a7c1a0e7bd74d5411b3cdcd02dcc6bc
alexinea <forerunner@outlook.com>
2020-08-17 17:35:23 +0800

    Refactor CountryManager

--- src/Cosmos.I18N.Countries/Cosmos/I18N/Countries/CountryManager.cs
+++ src/Cosmos.I18N.Countries/Cosmos/I18N/Countries/CountryManager.cs
@@ -1,24 +1,23 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Cosmos.I18N.Countries {
+namespace Cosmos.I18N.Countries
+{
     /// <summary>
     /// Country Manager
     /// </summary>
-    public static partial class CountryManager {
+    public static partial class CountryManager
+    {
         // ReSharper disable once InconsistentNaming
         private static readonly Dictionary<Country, CountryCode> _nameAndCodeMap;
 
         // ReSharper disable once InconsistentNaming
         private static readonly Dictionary<CountryCode, Country> _codeAndNameMap;
 
-        // ReSharper disable once InconsistentNaming
-        private static readonly Dictionary<Country, CountryInfo> _countryInfoCache;
-
-        static CountryManager() {
-            _nameAndCodeMap = InitOpts.Map1;
-            _codeAndNameMap = InitOpts.Map2;
-            _countryInfoCache = InitOpts.Map3;
+        static CountryManager()
+        {
+            _nameAndCodeMap = WholeWorld.Data.ToDictionary(k => k.Country, v => v.CountryCode);
+            _codeAndNameMap = WholeWorld.Data.ToDictionary(k => k.CountryCode, v => v.Country);
         }
 
         /// <summary>
@@ -26,8 +25,9 @@ namespace Cosmos.I18N.Countries {
         /// </summary>
         /// <param name="country"></param>
         /// <returns></returns>
-        public static CountryInfo GetCountryInfo(Country country) {
-            return _countryInfoCache[country];
+        public static CountryInfo GetCountryInfo(Country country)
+        {
+            return WholeWorld.Data.FirstOrDefault(x => x.Country == country);
         }
 
         /// <summary>
@@ -35,9 +35,9 @@ namespace Cosmos.I18N.Countries {
         /// </summary>
         /// <param name="code"></param>
         /// <returns></returns>
-        public static CountryInfo GetCountryInfo(CountryCode code) {
-            var country = _codeAndNameMap[code];
-            return _countryInfoCache[country];
+        public static CountryInfo GetCountryInfo(CountryCode code)
+        {
+            return WholeWorld.Data.FirstOrDefault(x => x.CountryCode == code);
         }
 
         /// <summary>
@@ -45,22 +45,38 @@ namespace Cosmos.I18N.Countries {
         /// </summary>
         /// <param name="alphaCode"></param>
         /// <returns></returns>
-        public static CountryInfo GetCountryInfo(string alphaCode) {
-            if (string.IsNullOrWhiteSpace(alphaCode)) {
+        public static CountryInfo GetCountryInfo(string alphaCode)
+        {
+            if (string.IsNullOrWhiteSpace(alphaCode))
+            {
                 return null;
             }
 
-            if (alphaCode.Length == 2) {
+            if (alphaCode.Length == 2)
+            {
                 var alpha2Code = alphaCode.ToUpper();
-                return _countryInfoCache.Values.FirstOrDefault(x => x.Alpha2Code == alpha2Code);
+                return WholeWorld.Data.FirstOrDefault(x => x.Alpha2Code == alpha2Code);
             }
 
-            if (alphaCode.Length == 3) {
+            if (alphaCode.Length == 3)
+            {
                 var alpha3Code = alphaCode.ToUpper();
-                return _countryInfoCache.Values.FirstOrDefault(x => x.Alpha3Code == alpha3Code);
+                return WholeWorld.Data.FirstOrDefault(x => x.Alpha3Code == alpha3Code);
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Gets <see cref="CountryInfo"/> via Cosmos Region Code / CEP-1.
+        /// </summary>
+        /// <param name="crcode"></param>
+        /// <returns></returns>
+        public static CountryInfo GetCountryInfo(long crcode)
+        {
+            //TODO 判断是否为历史代码
+
+            return WholeWorld.Data.FirstOrDefault(x => x.CRCode == crcode);
         }
 
         /// <summary>
@@ -68,7 +84,8 @@ namespace Cosmos.I18N.Countries {
         /// </summary>
         /// <param name="code"></param>
         /// <returns></returns>
-        public static Country GetCountryEnum(CountryCode code) {
+        public static Country GetCountryEnum(CountryCode code)
+        {
             return _codeAndNameMap[code];
         }
 
@@ -77,7 +94,8 @@ namespace Cosmos.I18N.Countries {
         /// </summary>
         /// <param name="country"></param>
         /// <returns></returns>
-        public static CountryCode GetCountryCode(Country country) {
+        public static CountryCode GetCountryCode(Country country)
+        {
             return _nameAndCodeMap[country];
         }
 
@@ -86,7 +104,8 @@ namespace Cosmos.I18N.Countries {
         /// </summary>
         /// <param name="country"></param>
         /// <returns></returns>
-        public static Continent GetContinent(Country country) {
+        public static Continent GetContinent(Country country)
+        {
             return GetCountryInfo(country).Continent;
         }
 
@@ -95,8 +114,265 @@ namespace Cosmos.I18N.Countries {
         /// </summary>
         /// <param name="code"></param>
         /// <returns></returns>
-        public static Continent GetContinent(CountryCode code) {
+        public static Continent GetContinent(CountryCode code)
+        {
             return GetCountryInfo(code).Continent;
+        }
+
+        private static class WholeWorld
+        {
+            public static readonly List<CountryInfo> Data = new List<CountryInfo>
+            {
+                Europe.Andorra.Instance,
+                Asia.Afghanistan.Instance,
+                Africa.Algeria.Instance,
+                NorthAmerica.AntiguaAndBarbuda.Instance,
+                NorthAmerica.Anguilla.Instance,
+                Asia.Armenia.Instance,
+                Africa.Angola.Instance,
+                CountryOf.Antarctica,
+                SouthAmerica.Argentina.Instance,
+                Oceania.AmericanSamoa.Instance,
+                Europe.Austria.Instance,
+                Oceania.Australia.Instance,
+                NorthAmerica.Aruba.Instance,
+                Europe.AlandIslands.Instance,
+                Asia.Azerbaijan.Instance,
+                Europe.BosniaAndHerzegovina.Instance,
+                NorthAmerica.Barbados.Instance,
+                Asia.Bangladesh.Instance,
+                Europe.Belgium.Instance,
+                Africa.BurkinaFaso.Instance,
+                Europe.Bulgaria.Instance,
+                Asia.Bahrain.Instance,
+                Africa.Burundi.Instance,
+                Africa.Benin.Instance,
+                NorthAmerica.Bermuda.Instance,
+                Asia.Brunei.Instance,
+                SouthAmerica.Bolivia.Instance,
+                NorthAmerica.Bonaire.Instance,
+                Europe.BouvetIsland.Instance,
+                SouthAmerica.Brazil.Instance,
+                NorthAmerica.Bahamas.Instance,
+                Asia.Bhutan.Instance,
+                Africa.Botswana.Instance,
+                Europe.Belarus.Instance,
+                NorthAmerica.Belize.Instance,
+                Africa.Cameroun.Instance,
+                NorthAmerica.Canada.Instance,
+                Africa.CapeVerde.Instance,
+                Africa.CentralAfrican.Instance,
+                Asia.China.Instance,
+                SouthAmerica.Chile.Instance,
+                Asia.ChristmasIsland.Instance,
+                Africa.CongoBrazzaville.Instance,
+                Africa.CongoKinshasa.Instance,
+                Asia.CocosIslands.Instance,
+                SouthAmerica.Colombia.Instance,
+                Oceania.CookIslands.Instance,
+                NorthAmerica.CostaRica.Instance,
+                Africa.CotedIvoire.Instance,
+                NorthAmerica.Cuba.Instance,
+                NorthAmerica.Curaçao.Instance,
+                Asia.Cyprus.Instance,
+                Europe.Czechia.Instance,
+                Europe.Denmark.Instance,
+                NorthAmerica.Dominica.Instance,
+                NorthAmerica.Dominican.Instance,
+                Asia.Djibouti.Instance,
+                SouthAmerica.Ecuador.Instance,
+                Europe.Estonia.Instance,
+                Africa.Egypt.Instance,
+                Africa.Eritrea.Instance,
+                Africa.Ethiopia.Instance,
+                SouthAmerica.FalklandIslands.Instance,
+                Europe.Faroe.Instance,
+                Oceania.Fiji.Instance,
+                Europe.Finland.Instance,
+                Europe.France.Instance,
+                Asia.Georgia.Instance,
+                Europe.Germany.Instance,
+                NorthAmerica.Grenada.Instance,
+                SouthAmerica.Guiana.Instance,
+                Africa.Guinea.Instance,
+                Africa.EquatorialGuinea.Instance,
+                NorthAmerica.Guadeloupe.Instance,
+                NorthAmerica.Greenland.Instance,
+                Africa.Gabon.Instance,
+                Africa.Ghana.Instance,
+                Europe.Gibraltar.Instance,
+                Africa.Gambia.Instance,
+                NorthAmerica.Guatemala.Instance,
+                Europe.Greece.Instance,
+                Oceania.Guam.Instance,
+                Africa.GuineaBissau.Instance,
+                SouthAmerica.Guyana.Instance,
+                Asia.HIMI.Instance,
+                Asia.HongKong.Instance,
+                SouthAmerica.Honduras.Instance,
+                Europe.Vatican.Instance,
+                Europe.Croatia.Instance,
+                NorthAmerica.Haiti.Instance,
+                Europe.Hungary.Instance,
+                Asia.India.Instance,
+                Asia.Indonesia.Instance,
+                Asia.Israel.Instance,
+                Asia.BritishIndianOceanTerritory.Instance,
+                Asia.Iraq.Instance,
+                Asia.Iran.Instance,
+                Europe.Iceland.Instance,
+                Europe.Ireland.Instance,
+                Europe.Italy.Instance,
+                NorthAmerica.Jamaica.Instance,
+                Asia.Japan.Instance,
+                Europe.Jersey.Instance,
+                Asia.Jordan.Instance,
+                Africa.Kenya.Instance,
+                Oceania.Kiribati.Instance,
+                Africa.Comoros.Instance,
+                Asia.Kyrgyzstan.Instance,
+                Asia.SouthKorea.Instance,
+                Asia.NorthKorea.Instance,
+                Asia.Cambodia.Instance,
+                NorthAmerica.CaymanIslands.Instance,
+                Asia.Kuwait.Instance,
+                Asia.Kazakhstan.Instance,
+                Asia.LaoPDR.Instance,
+                Asia.Lebanon.Instance,
+                NorthAmerica.SaintLucia.Instance,
+                Europe.Liechtenstein.Instance,
+                Asia.SriLanka.Instance,
+                Africa.Liberia.Instance,
+                Africa.Lesotho.Instance,
+                Europe.Lithuania.Instance,
+                Europe.Luxembourg.Instance,
+                Europe.Latvia.Instance,
+                Africa.Libya.Instance,
+                Asia.Macao.Instance,
+                Europe.Mann.Instance,
+                Africa.Mayotte.Instance,
+                Oceania.Micronesia.Instance,
+                Africa.Morocco.Instance,
+                Europe.Monaco.Instance,
+                Europe.Moldova.Instance,
+                Europe.Montenegro.Instance,
+                NorthAmerica.SaintMartinFrenchPart.Instance,
+                NorthAmerica.SintMaartenDutchPart.Instance,
+                Africa.Madagascar.Instance,
+                Oceania.MarshallIslands.Instance,
+                Europe.Macedonia.Instance,
+                Africa.Mali.Instance,
+                NorthAmerica.Mexico.Instance,
+                Asia.Myanmar.Instance,
+                Asia.Mongolia.Instance,
+                Oceania.NorthernMarianaIslands.Instance,
+                NorthAmerica.Martinique.Instance,
+                Africa.Mauritania.Instance,
+                NorthAmerica.Montserrat.Instance,
+                Europe.Malta.Instance,
+                Africa.Mauritius.Instance,
+                Asia.Maldives.Instance,
+                Africa.Malawi.Instance,
+                Asia.Malaysia.Instance,
+                Africa.Mozambique.Instance,
+                Africa.Namibia.Instance,
+                Oceania.NewCaledonia.Instance,
+                Europe.Netherlands.Instance,
+                Oceania.NewZealand.Instance,
+                SouthAmerica.Nicaragua.Instance,
+                Africa.Niger.Instance,
+                Africa.Nigeria.Instance,
+                Oceania.NorfolkIsland.Instance,
+                Asia.Nepal.Instance,
+                Oceania.Nauru.Instance,
+                Oceania.Niue.Instance,
+                Europe.Norway.Instance,
+                Asia.Oman.Instance,
+                NorthAmerica.Panama.Instance,
+                SouthAmerica.Paraguay.Instance,
+                SouthAmerica.Peru.Instance,
+                Oceania.Polynesia.Instance,
+                Oceania.PapuaNewGuinea.Instance,
+                Asia.Philippines.Instance,
+                Asia.Pakistan.Instance,
+                Europe.Poland.Instance,
+                NorthAmerica.SaintPierreAndMiquelon.Instance,
+                Oceania.Pitcairn.Instance,
+                NorthAmerica.PuertoRico.Instance,
+                Asia.Palestine.Instance,
+                Europe.Portugal.Instance,
+                Oceania.Palau.Instance,
+                Asia.Qatar.Instance,
+                Africa.Réunion.Instance,
+                Europe.Romania.Instance,
+                Europe.Russia.Instance,
+                Africa.Rwanda.Instance,
+                SouthAmerica.SGSSI.Instance,
+                NorthAmerica.SaintBarts.Instance,
+                Oceania.Samoa.Instance,
+                Asia.SaudiArabia.Instance,
+                NorthAmerica.SaintKittsAndNevis.Instance,
+                NorthAmerica.StVAndG.Instance,
+                Europe.Serbia.Instance,
+                Oceania.SolomonIslands.Instance,
+                Africa.Seychelles.Instance,
+                Africa.Sudan.Instance,
+                Europe.Sweden.Instance,
+                Asia.Singapore.Instance,
+                Europe.Slovenia.Instance,
+                Africa.SaintHelena.Instance,
+                Europe.SvalbardAndJanMayen.Instance,
+                Europe.Slovakia.Instance,
+                Africa.SierraLeone.Instance,
+                Europe.SanMarino.Instance,
+                Africa.Senegal.Instance,
+                Africa.Somalia.Instance,
+                Africa.Suriname.Instance,
+                Africa.SouthSudan.Instance,
+                Africa.SouthAfrica.Instance,
+                Africa.SaoTomeAndPrincipe.Instance,
+                Europe.Spain.Instance,
+                Europe.Switzerland.Instance,
+                Asia.Syrian.Instance,
+                Africa.Swaziland.Instance,
+                NorthAmerica.ElSalvador.Instance,
+                Asia.Taiwan.Instance,
+                Africa.Tanzania.Instance,
+                NorthAmerica.TrinidadAndTobago.Instance,
+                NorthAmerica.TurksAndCaicosIslands.Instance,
+                Africa.Chad.Instance,
+                Africa.FrenchSouthernTerritories.Instance,
+                Africa.Togo.Instance,
+                Asia.Thailand.Instance,
+                Asia.Tajikistan.Instance,
+                Oceania.Tokelau.Instance,
+                Asia.EastTimor.Instance,
+                Asia.Turkmenistan.Instance,
+                Africa.Tunisia.Instance,
+                Oceania.Tonga.Instance,
+                Asia.Turkey.Instance,
+                Oceania.Tuvalu.Instance,
+                Africa.Uganda.Instance,
+                Europe.Ukraine.Instance,
+                Asia.UnitedArabEmirates.Instance,
+                Europe.UnitedKingdom.Instance,
+                NorthAmerica.UnitedStates.Instance,
+                NorthAmerica.UnitedStatesMinorOutlyingIslands.Instance,
+                SouthAmerica.Uruguay.Instance,
+                Asia.Uzbekistan.Instance,
+                SouthAmerica.Venezuela.Instance,
+                Asia.Vietnam.Instance,
+                NorthAmerica.VirginIslandsBritish.Instance,
+                NorthAmerica.VirginIslandsUS.Instance,
+                Oceania.Vanuatu.Instance,
+                Oceania.WallisAndFutuna.Instance,
+                Africa.WesternSahara.Instance,
+                Europe.Kosovo.Instance,
+                Asia.Yemen.Instance,
+                Europe.Yugoslavia1992.Instance,
+                Africa.Zambia.Instance,
+                Africa.Zimbabwe.Instance,
+            };
         }
     }
 }
