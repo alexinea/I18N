9da0414e8f157abc5b1ace6aa2e095ce8711b555
alexinea <forerunner@outlook.com>
2020-09-08 15:41:19 +0800

    Add dependency starter.

---
+++ src/Cosmos.I18N.Extensions.Autofac/Cosmos/I18N/Extensions.HistoricalSupport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Cosmos.I18N.Core;
+using Cosmos.I18N.Core.Historical;
+using Microsoft.Extensions.Configuration;
+
+namespace Cosmos.I18N
+{
+    public static class HistoricalSupportExtensions
+    {
+        public static II18NServiceCollection AddHistoricalSupport(this II18NServiceCollection services, IConfigurationSection config)
+        {
+            var models = config.GetValue<IEnumerable<HistoricalCountryModel>>("Historical");
+
+            HistoricalCountryStarter.AppendConfiguredCountries(models);
+
+            return services;
+        }
+    }
+}
